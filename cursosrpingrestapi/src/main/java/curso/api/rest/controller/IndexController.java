0c68908ae50a6f0a02cf2362239821031b631c73
Lenovo <Lenovo@DESKTOP-MRA2L5M>
2019-05-12 20:52:32 -0300

    Adicionando funcionalidades de editar e deletar usuário. Api RestFull de CRUD de usuarios finalizada

--- cursosrpingrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursosrpingrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,10 +6,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.modelo.Usuario;
@@ -35,7 +38,25 @@ public class IndexController {
 		
 		List<Usuario> users = (List<Usuario>) usuarioRepository.findAll();
 		
-		return new ResponseEntity<List<Usuario>>(users, HttpStatus.OK);
+		return new ResponseEntity<List<Usuario>>(users, HttpStatus.OK);	
+	}
+	
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> insertUser (@RequestBody Usuario usuario){
+		Usuario userSave = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(userSave, HttpStatus.OK);
+	}
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> editUser (@RequestBody Usuario usuario){
+		Usuario userEdit = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(userEdit, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public ResponseEntity deleteUser (@PathVariable (value= "id") Long id){
 		
+		usuarioRepository.deleteById(id);
+		return new ResponseEntity ("Usuario deletado com sucesso", HttpStatus.OK);
 	}
 }
